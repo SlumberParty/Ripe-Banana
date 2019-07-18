48da1b4eb00802aa2911d0e194acbe755ff0d215
Claire Cassidy <clacassidy@gmail.com>
2019-07-18 13:17:49 -0700

    made film put request and test, borked everything, trying to fix

---
+++ test/film.test.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Actor = require('../lib/models/Actor');
+const Studio - require('../lib/models/Studio');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+  
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+  
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  
+  let actor = null;
+  beforeEach(async() => {
+    actor = JSON.parse(JSON.stringify(await Actor.create({ name: 'ahh' })));
+  });
+
+  let studio = null;
+  beforeEach(async() => {
+    studio = JSON.parse(JSON.stringify(await Studio.create({ name: 'ahh' })));
+  });
+
+  it('can create a film', () => {
+    return request(app)
+      .post('/api/v1/films')
+      .send({ 
+        title: 'ahh', 
+        studio: studio._id, 
+        released: 2004, 
+        cast: [{ 
+          role: 'ahh', 
+          actor: actor._id 
+        }] 
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'BlahBlah',
+          studio: 'ahh',
+          released: 2004, 
+          cast: [{ 
+            role: 'ahh', 
+            actor: expect.any(String),
+          }],
+          __v: 0
+        });
+      });  
+  });
+});
