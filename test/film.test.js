32dfda7f8550f93905dff39425ad024a55801576
Claire Cassidy <clacassidy@gmail.com>
2019-07-18 16:10:38 -0700

    passed film get all test

--- test/film.test.js
+++ test/film.test.js
@@ -6,6 +6,7 @@ const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Actor = require('../lib/models/Actor');
 const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -56,5 +57,58 @@ describe('app routes', () => {
           __v: 0
         });
       });  
+  });
+
+  it('get some films', async() => {
+    const films = await Film.create([
+      {
+        title: 'ahh', 
+        studio: studio._id, 
+        released: 2004, 
+        cast: [{ 
+          role: 'ahh', 
+          actor: actor._id,
+        }]
+      },
+      {
+        title: 'ahh2', 
+        studio: studio._id, 
+        released: 2004, 
+        cast: [{ 
+         
+          role: 'ahh', 
+          actor: actor._id,
+        }]
+      },
+      {
+        title: 'ahh3', 
+        studio: studio._id, 
+        released: 2004, 
+        cast: [{
+          role: 'ahh', 
+          actor: actor._id,
+        }]
+      }
+    ]);
+
+    return request(app)
+      .get('/api/v1/films')
+      .then(res => {
+        const filmsJSON = JSON.parse(JSON.stringify(films));
+        filmsJSON.forEach(film => {
+          console.log(film);
+          expect(res.body).toContainEqual({
+            _id: film._id,
+            title: film.title, 
+            studio: studio._id, 
+            released: 2004, 
+            cast: [{ 
+              _id: expect.any(String),
+              role: 'ahh', 
+              actor: actor._id,
+            }]
+          });
+        });
+      });
   });
 });
